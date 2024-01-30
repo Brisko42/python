07f667f6a45af99042c1ff1a8fdc6c6cff05ba6e
Michel <104710165+Brisko42@users.noreply.github.com>
2024-01-30 15:28:40 +0100

    Delete exercice_vagrant/.vagrant directory

--- exercice_vagrant/.vagrant/plugins/gems/3.1.3/gems/vagrant-timezone-1.3.0/lib/vagrant-timezone.rb
+++
@@ -1,2 +0,0 @@
-require_relative 'vagrant-timezone/version'
-require_relative 'vagrant-timezone/plugin'
--- exercice_vagrant/.vagrant/plugins/gems/3.1.3/gems/vagrant-timezone-1.3.0/lib/vagrant-timezone/version.rb
+++
@@ -1,5 +0,0 @@
-module VagrantPlugins
-  module TimeZone
-    VERSION = '1.3.0'.freeze
-  end
-end
